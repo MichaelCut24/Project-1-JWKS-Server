de3dc102819acb946f25243fa754b191118be35d
MichaelCut24 <MichaelCutbirth@my.unt.edu>
2024-03-24 22:18:14 -0500

    project 2

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('totally_not_my_privateKeys.db')
 
 
 //kID for each auth token inspired by Street Fighter Characters
@@ -28,30 +31,35 @@ const kID = [
     }
 ]
 
+
+
 //Shows correct data if you have the right auth code
 app.get('/test', check, (req, res) => {
     res.json(kID.filter(kID => kID.user === req.ID.KID))
 })
 
+
 function check(req, res, next) {
     //For using already
-    const authHead = req.headers['authorization']
-    const tokenChecker = authHead && authHead.split(' ')[1]
+   //const authHead = req.headers['authorization']
+    //const tokenChecker = authHead && authHead.split(' ')[1]
     //Error code if nothing is put into the auth
-    if (tokenChecker == null) return res.sendStatus(401)
+    //if (tokenChecker == null) return res.sendStatus(401)
 
     //Verify code hasn't expired
     jwt.verify(tokenChecker, process.env.TOKEN, (err, ID) =>{
         //if code is invalid or expired than throws forbidden error
-        if (err) return res.sendStatus(403)
+        if (err) return res.sendStatus(404)
         req.ID = ID
         next()
     })
 }
 
+
+
 //Refresh token good for 15 seconds
 function tokenExpires(ID){
-    return jwt.sign(ID, process.env.TOKEN, { expiresIn: '15s'})
+    return jwt.sign(ID, process.env.TOKEN, { expiresIn: '3600s'})
 }
 
 //post /auth
@@ -64,10 +72,20 @@ app.post('/auth', async (req, res) =>{
     const ID = { KID : user}
 
     //calling token and setting the token to a Token (tokeception)
-    const Token = tokenExpires(ID)
 
+    const Token = db.each("SELECT exp FROM keys")
     res.json({Token: Token})
+
+
+    db.serialize(() => {
+        const T0ken = process.env.TOKEN;
+        db.run("CREATE TABLE IF NOT EXISTS keys ( kid BLOB PRIMARY KEY AUTOINCREMENT, k3y BLOB NOT NULL, exp BLOB NOT NULL)")
+        db.run("INSERT INTO keys (kid, k3y, exp) VALUES("+T0ken+"'," +ID+",'"+tokenExpires(ID)+ "')")
+    });
+    db.close();
+
 });
+
 
 
 //Better listener than me
